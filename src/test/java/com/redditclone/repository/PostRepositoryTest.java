f7715f2ae235328482650555a6fc0538a424cdbc
KiranGajul <kirangajul1998@gmail.com>
2023-08-05 15:32:26 +0530

    Added all unit tests

---
+++ src/test/java/com/redditclone/repository/PostRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.redditclone.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.redditclone.BaseTest;
+import com.redditclone.model.Post;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PostRepositoryTest extends BaseTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void shouldSavePost() {
+        Post expectedPostObject = new Post(null, "First Post", "http://url.site", "Test",
+                0, null, Instant.now(), null);
+        Post actualPostObject = postRepository.save(expectedPostObject);
+        assertThat(actualPostObject).usingRecursiveComparison()
+                .ignoringFields("postId").isEqualTo(expectedPostObject);
+    }
+
+}
